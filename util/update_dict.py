87efa98472b421b9d8efc8c8b43f6562b61a0a83
steagray <steagray@ut.utm.edu>
2023-04-23 00:28:48 -0500

    saves dictionary, not gzip file

--- util/update_dict.py
+++ util/update_dict.py
@@ -1,13 +1,23 @@
 import requests
 import gzip
+from os import remove
 
-# Dictionaries: JMdict (w/ examples), Kanjidic
+# URL to dictionaries to download 
+# JMdict (w/ examples)
 dicts = ["http://ftp.edrdg.org/pub/Nihongo/JMdict_e_examp.gz"]
 
+# For each entry in the dicts array, downloads the gz file, and then decompresses it
+# Uses the final segment of the url to determine the filename
 def update_dicts():
     for url in dicts:
         words = url.split('/')
         req = requests.get(url, allow_redirects=True)
+        if req.status_code != requests.codes.ok:
+            print("Unable to get ", words[-1])
+            return
+
         open('dicts/' + words[-1], 'wb').write(req.content)
-        
-        open('dicts/test', 'wb'.write(gzip.decompress(gzip.open('dicts/' + words[-1], 'wb')))
+        with gzip.open('dicts/' + words[-1], 'rb') as g:
+            with open('dicts/' + words[-1][:-3:1], 'wb') as f:
+                f.write(g.read())
+        remove("dicts/" + words[-1])
